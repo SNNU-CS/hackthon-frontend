b2358d275b2b8d3f27f20c16e9080d730d452dbb
Yubing Wei <40016996+biuiko@users.noreply.github.com>
2019-04-20 20:06:22 +0800

    add true data and add a page of activity detail (#12)
    连接数据库，首页使用数据库真实数据
    增加查看详细活动页面，并增加Dialog组件

--- hackthon/pages/activityDetail/activityDetail.js
+++ hackthon/pages/activityDetail/activityDetail.js
@@ -1,3 +1,5 @@
+import Dialog from '../../miniprogram_npm/vant-weapp/dialog/dialog';
+
 Page({
 
   /**
@@ -5,6 +7,33 @@ Page({
    */
   data: {
     id:null
+  },
+  /**
+   * 点击加入活动
+   */
+  handleSubmit:function(){
+    Dialog.confirm({
+      title: '确认信息',
+      message: '你确认加入本次活动吗qwq确认后不可以取消的哦'
+    })
+  },
+/**
+ * dialog确认和取消按钮绑定的事件
+ */
+  onCancle:function(){
+    Dialog.close()
+    
+  },
+  onConfirm: function () {
+    wx.showLoading({
+      title: '正在提交'
+    })
+    //修改数据库信息 需要人数-- 现有人数++
+    wx.hideLoading()
+    Dialog.close()
+    wx.redirectTo({
+      url: '/pages/activityDetail/activityDetail?activityUserId=50',
+    })
   },
 
   /**
