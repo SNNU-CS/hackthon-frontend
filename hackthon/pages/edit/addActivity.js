caa868d13583d9c1fb417c353dc0843215b9c225
tender-boluo <925591914@qq.com>
2019-04-20 22:55:47 +0800

    增加 “组局” 相关事件 (#15)

--- hackthon/pages/edit/addActivity.js
+++ hackthon/pages/edit/addActivity.js
@@ -5,62 +5,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-    title: "新建活动！"
+    title: "组局！",
+    param: { "title": "",
+           "description": "",
+           "number": "",
+           "location": "",},
+    time: new Date().getTime(),
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onInput(event) {
+    var date = new Date(event.detail);
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+    var D = date.getDate() + ' ';
+    var h = date.getHours() + ':';
+    var m = date.getMinutes() + ':';
+    var s = date.getSeconds();
+    this.data.time = Y + M + D + h + m + s;
+    wx.showToast({
+      title: '时间确认成功',
+      icon: 'success',
+      duration: 1000
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  addActivity: function () {
+    wx.request({
+      url: 'https://test.xiekeyi98.com/activity/',
+      data: JSON.stringify({
+            title: this.data.param['title'],
+            description: this.data.param['description'],
+            time: this.data.time,
+            // number: this.data.param['number'],
+            location: this.data.param['location']}),
+      method: 'POST',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }, // 设置请求的 header
+      success: function (res) {
+        if(res.data.status == 403){
+          wx.showModal({
+            content: '请将活动内容填写完整',
+            showCancel: false,
+          })
+        } else {
+          wx.showModal({
+            title: '组局成功~',
+            content: '将去往个人中心',
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                wx.switchTab({
+                  url: '../personal_center/index',
+                });
+              }
+            }
+          })
+        }
+        // success
+      },
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onChange(event) {
+    this.data.param[event.currentTarget.id] = event.detail;
   }
 })
