8eedeb3931350a1004bd0a9e2b29097b9c4c8f79
Yubing Wei <40016996+biuiko@users.noreply.github.com>
2019-04-20 22:09:31 +0800

    add activity detail page (#14)

--- hackthon/pages/activityDetail/activityDetail.js
+++ hackthon/pages/activityDetail/activityDetail.js
@@ -6,8 +6,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    id:null
+    id:null,
+    openid:null
   },
+  /**
+   * 获取用户id
+   */
+  onLoad: function () {
+    var that = this
+    wx.login({
+      success: function (res) {
+        console.log("res.code=====" + res.code);
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'http://148.70.15.188:8000',
+            method: "POST",
+            success: function (res) {
+              that.setData({
+                openid: res.data.openid
+              })
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
+  },
+
+
   /**
    * 点击加入活动
    */
@@ -16,6 +44,7 @@ Page({
       title: '确认信息',
       message: '你确认加入本次活动吗qwq确认后不可以取消的哦'
     })
+    console.log(this.data.openid)
   },
 /**
  * dialog确认和取消按钮绑定的事件
@@ -26,7 +55,7 @@ Page({
   },
   onConfirm: function () {
     wx.showLoading({
-      title: '正在提交'
+      title: '正在提交请求'
     })
     //修改数据库信息 需要人数-- 现有人数++
     wx.hideLoading()
